52595b8136f40730bb5fa9e2b1cc445511b10e2a
MJ <793650314@qq.com>
2018-04-13 15:53:08 +0800

    api/user

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Http\Request;
 
 $api = app('Dingo\Api\Routing\Router');
 
-$api->version('v1', ['namespace' => 'App\Http\Controllers\Api'], function($api) {
+$api->version('v1', ['namespace' => 'App\Http\Controllers\Api','middleware' => 'serializer:array'], function($api) {
 
     $api->group([
         'middleware' => 'api.throttle',
@@ -45,7 +45,20 @@ $api->version('v1', ['namespace' => 'App\Http\Controllers\Api'], function($api) 
             ->name('api.authorizations.destroy');
     });
 
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.access.limit'),
+        'expires' => config('api.rate_limits.access.expires'),
+    ], function ($api) {
+        // 游客可以访问的接口
 
+        // 需要 token 验证的接口
+        $api->group(['middleware' => 'api.auth'], function($api) {
+            // 当前登录用户信息
+            $api->get('user', 'UsersController@me')
+                ->name('api.user.show');
+        });
+    });
 
 
     // 上传文件
