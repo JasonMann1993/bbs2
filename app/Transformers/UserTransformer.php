6bd57ac54018ef906c0bf3766556f8a98058c411
MJ <793650314@qq.com>
2018-04-16 16:25:50 +0800

    include user rolers

--- app/Transformers/UserTransformer.php
+++ app/Transformers/UserTransformer.php
@@ -8,9 +8,10 @@ use League\Fractal\TransformerAbstract;
 
 class UserTransformer extends TransformerAbstract
 {
+    protected $availableIncludes = ['roles'];
+
     public function transform(User $user)
     {
-        Carbon::now()->toDateTimeString();
         return [
             'id' => $user->id,
             'name' => $user->name,
@@ -23,5 +24,10 @@ class UserTransformer extends TransformerAbstract
             'created_at' => $user->created_at->toDateTimeString(),
             'updated_at' => $user->updated_at->toDateTimeString(),
         ];
+    }
+
+    public function includeRoles(User $user)
+    {
+        return $this->collection($user->roles, new RoleTransformer());
     }
 }
